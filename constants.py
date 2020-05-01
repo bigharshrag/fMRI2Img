31a5b5429538293b76b0c2ae23b7c324fcbf5c01
Rishabh Garg <rishabh@cs.utexas.edu>
2020-05-01 00:02:51 -0500

    Only GAN without immediate mode collapse

--- constants.py
+++ constants.py
@@ -5,7 +5,7 @@ class BaseOptions():
         self.parser = argparse.ArgumentParser()
         self.parser.add_argument('--exp_name', type=str, default='exp1', help='Name of the experiment')
         self.parser.add_argument('--dataroot', type=str, default='data', help='Root directory for dataset')
-        self.parser.add_argument('--workers', default=4, type=int, help='Number of workers for dataloader')
+        self.parser.add_argument('--workers', default=8, type=int, help='Number of workers for dataloader')
         self.parser.add_argument('--batch_size', default=16, type=int, help='Batch size during training')
         self.parser.add_argument('--ngpu', default=1, type=int, help='Size of feature maps in discriminator')
 
@@ -16,12 +16,13 @@ class BaseOptions():
         self.parser.add_argument('--ndf', default=64, type=int, help='Size of feature maps in discriminator')
 
         self.parser.add_argument('--num_epochs', default=10, type=int, help='Number of training epochs')
-        self.parser.add_argument('--lr', default=0.0001, type=float, help='Learning rate for optimizers')
-        self.parser.add_argument('--beta1', default=0.5, type=float, help='Beta1 hyperparam for Adam optimizers')
+        self.parser.add_argument('--g_lr', default=0.00005, type=float, help='Learning rate for optimizers')
+        self.parser.add_argument('--d_lr', default=0.00005, type=float, help='Learning rate for optimizers')
+        self.parser.add_argument('--beta1', default=0.9, type=float, help='Beta1 hyperparam for Adam optimizers')
         
-        self.parser.add_argument('--lambda_1', default=2000000, type=float, help='Beta1 hyperparam for Adam optimizers')
-        self.parser.add_argument('--lambda_2', default=0.01, type=float, help='Beta1 hyperparam for Adam optimizers')
-        self.parser.add_argument('--lambda_3', default=100, type=float, help='Beta1 hyperparam for Adam optimizers')
+        self.parser.add_argument('--lambda_1', default=0, type=float, help='Beta1 hyperparam for Adam optimizers')
+        self.parser.add_argument('--lambda_2', default=0, type=float, help='Beta1 hyperparam for Adam optimizers')
+        self.parser.add_argument('--lambda_3', default=1, type=float, help='Beta1 hyperparam for Adam optimizers')
             
         self.parser.add_argument('--snapshot_interval', default=5)
 
--- train.py
+++ train.py
@@ -30,9 +30,17 @@ generator = Generator(ngpu, tfmri.shape[0]).to(device)
 if (device.type == 'cuda') and (ngpu > 1):
     generator = nn.DataParallel(generator, list(range(ngpu)))
 
+def weights_init(m):
+    classname = m.__class__.__name__
+    if classname.find('Conv') != -1:
+        nn.init.normal_(m.weight.data, 0.0, 0.02)
+    elif classname.find('BatchNorm') != -1:
+        nn.init.normal_(m.weight.data, 1.0, 0.02)
+        nn.init.constant_(m.bias.data, 0)
+
 # Apply the weights_init function to randomly initialize all weights
 #  to mean=0, stdev=0.2.
-# generator.apply(weights_init)
+generator.apply(weights_init)
 
 # Print the model
 # print(generator)
@@ -46,7 +54,7 @@ if (device.type == 'cuda') and (ngpu > 1):
 
 # Apply the weights_init function to randomly initialize all weights
 #  to mean=0, stdev=0.2.
-# discriminator.apply(weights_init)
+discriminator.apply(weights_init)
 
 # Print the model
 # print(discriminator)
@@ -60,8 +68,8 @@ real_label = 1
 generated_label = 0
 
 # Setup Adam optimizers for both G and D
-optimizerD = optim.Adam(discriminator.parameters(), lr=args.lr, betas=(args.beta1, 0.999))
-optimizerG = optim.Adam(generator.parameters(), lr=args.lr, betas=(args.beta1, 0.999))
+optimizerD = optim.Adam(discriminator.parameters(), lr=args.d_lr, betas=(args.beta1, 0.999))
+optimizerG = optim.Adam(generator.parameters(), lr=args.g_lr, betas=(args.beta1, 0.999))
 
 # Training Loop
 train_discr = True
@@ -69,7 +77,7 @@ train_gen = True
 
 # bce_loss = nn.BCEWithLogitsLoss()
 bce_loss = nn.BCELoss()
-mse_loss = nn.MSELoss(reduction='sum')
+mse_loss = nn.MSELoss(reduction='mean')
 
 writer = SummaryWriter('runs/' + args.exp_name)
 
@@ -83,60 +91,47 @@ for it in range(args.num_epochs):
 
         total_steps += curr_sz
         # Feed the data (images) to the encoder and run it        
-        encoded_real_image = encoder.forward(data_image)
+        # encoded_real_image = encoder(data_image)
 
         # Feed the data to the generator and run it
-        generator_output = generator.forward(data_fmri)
-        generator_image_loss = mse_loss(generator_output, data_image)
+        generator_output = generator(data_fmri)
+        # print(generator_output.shape)
+        # generator_output_crop = generator_output[:, :, 16:240, 16:240].detach()
+        # generator_image_loss = mse_loss(generator_output_crop, data_image)
 
         # Feed the generated image into the encoder
-        encoded_generated_image = encoder.forward(generator_output)
-        generator_encoded_loss = mse_loss(encoded_generated_image, encoded_real_image)
+        # encoded_generated_image = encoder(generator_output_crop)
+        # generator_encoded_loss = mse_loss(encoded_generated_image, encoded_real_image)
 
         discriminator.zero_grad()
         # Run the discriminator on real image
-        real_classification = discriminator.forward(data_image).view(-1)
+        real_classification = discriminator(data_image).view(-1)
         real_labels = torch.full((curr_sz,), real_label, device=device)
         disc_loss_real = bce_loss(real_classification, real_labels)
         if train_discr:
             disc_loss_real.backward()
 
         # Run the discriminator on generated data
-        generated_classification = discriminator.forward(generator_output.detach()).view(-1)
+        generated_classification = discriminator(generator_output.detach()).view(-1)
         generated_labels = torch.full((curr_sz,), generated_label, device=device)
-        disc_loss_gen = bce_loss(generated_classification, real_labels)
+        disc_loss_gen = bce_loss(generated_classification, generated_labels)
         if train_discr:
             disc_loss_gen.backward()
 
-        optimizerD.step()
+            optimizerD.step()
 
         generator.zero_grad()
         # Run the discriminator on generated data with opposite labels, to get the gradient for the generator
-        generated_classification_for_generator = discriminator.forward(generator_output).view(-1)
+        generated_classification_for_generator = discriminator(generator_output).view(-1)
         real_labels_for_generator_loss = torch.full((curr_sz,), real_label, device=device)
         generator_disc_loss = bce_loss(generated_classification_for_generator, real_labels_for_generator_loss)
 
         if train_gen:
-            gen_loss = args.lambda_1 * generator_image_loss + args.lambda_2 * generator_encoded_loss + args.lambda_3 * generator_disc_loss
+            # gen_loss = args.lambda_1 * generator_image_loss + args.lambda_2 * generator_encoded_loss + args.lambda_3 * generator_disc_loss
+            gen_loss = generator_disc_loss
             gen_loss.backward()
     
-        optimizerG.step()
-
-        # Display info
-        print('========================================')
-        print('')
-        print('[%s] Iteration %d: %f seconds' % (time.strftime('%c'), it, time.time()-start))
-        print('  generator image loss: %e' % (generator_image_loss))
-        print('  generator feat loss: %e' % (generator_encoded_loss))
-        print('  discr real loss: %e' % (disc_loss_real))
-        print('  discr generated loss: %e' % (disc_loss_gen))
-        print('  generator loss from discriminator: %e' % (generator_disc_loss))
-        writer.add_scalar('data/generator_image_loss', generator_image_loss, total_steps)
-        writer.add_scalar('data/generator_feat_loss', generator_encoded_loss, total_steps)
-        writer.add_scalar('data/discr_real_loss', disc_loss_real, total_steps)
-        writer.add_scalar('data/discr_generated_loss', disc_loss_gen, total_steps)
-        writer.add_scalar('data/generator_loss', generator_disc_loss, total_steps)
-        start = time.time()
+            optimizerG.step()
 
         # Save snapshot
         if it % args.snapshot_interval == 0:
@@ -147,20 +142,43 @@ for it in range(args.num_epochs):
 
         # Switch optimizing discriminator and generator, so that neither of them overfits too much
         
-        discr_loss_ratio = (disc_loss_real + disc_loss_gen) / generator_disc_loss
+        # discr_loss_ratio = (disc_loss_real.item() + disc_loss_gen.item()) / generator_disc_loss.item()
         
-        if discr_loss_ratio < 1e-1 and train_discr:
-            train_discr = False
-            train_gen = True
-            print('<<< real_loss=%e, fake_loss=%e, fake_loss_for_generator=%e, train_discr=%d, train_gen=%d >>>' % (disc_loss_real, disc_loss_gen, generator_disc_loss, train_discr, train_gen))
-        if discr_loss_ratio > 5e-1 and not train_discr:
-            train_discr = True
-            train_gen = True
-            print(' <<< real_loss=%e, fake_loss=%e, fake_loss_for_generator=%e, train_discr=%d, train_gen=%d >>>' % (disc_loss_real, disc_loss_gen, generator_disc_loss, train_discr, train_gen))
-        if discr_loss_ratio > 1e1 and train_gen:
-            train_gen = False
-            train_discr = True
-            print('<<< real_loss=%e, fake_loss=%e, fake_loss_for_generator=%e, train_discr=%d, train_gen=%d >>>' % (disc_loss_real, disc_loss_gen, generator_disc_loss, train_discr, train_gen))
+        # if discr_loss_ratio < 1e-1 and train_discr:
+        #     train_discr = False
+        #     train_gen = True
+        #     print('<<< real_loss=%e, fake_loss=%e, fake_loss_for_generator=%e, train_discr=%d, train_gen=%d >>>' % (disc_loss_real, disc_loss_gen, generator_disc_loss.item(), train_discr, train_gen))
+        # if discr_loss_ratio > 5e-1 and not train_discr:
+        #     train_discr = True
+        #     train_gen = True
+        #     print(' <<< real_loss=%e, fake_loss=%e, fake_loss_for_generator=%e, train_discr=%d, train_gen=%d >>>' % (disc_loss_real, disc_loss_gen, generator_disc_loss.item(), train_discr, train_gen))
+        # if discr_loss_ratio > 1e1 and train_gen:
+        #     train_gen = False
+        #     train_discr = True
+        #     print('<<< real_loss=%e, fake_loss=%e, fake_loss_for_generator=%e, train_discr=%d, train_gen=%d >>>' % (disc_loss_real, disc_loss_gen, generator_disc_loss.item(), train_discr, train_gen))
+
+        # Display info
+        print('========================================')
+        print('')
+        print('[%s] Iteration %d: %f seconds' % (time.strftime('%c'), it, time.time()-start))
+        # print('  generator image loss: %e' % (generator_image_loss))
+        # print('  generator feat loss: %e' % (generator_encoded_loss))
+        print('  discr real loss: %e' % (disc_loss_real.item()))
+        print('  discr generated loss: %e' % (disc_loss_gen.item()))
+        print('  generator loss from discriminator: %e' % (generator_disc_loss.item()))
+        # writer.add_scalar('data/generator_image_loss', args.lambda_1 * generator_image_loss, total_steps)
+        # writer.add_scalar('data/generator_feat_loss', args.lambda_2 * generator_encoded_loss, total_steps)
+        writer.add_scalar('data/discr_real_loss', disc_loss_real.item(), total_steps)
+        writer.add_scalar('data/discr_generated_loss', disc_loss_gen.item(), total_steps)
+        writer.add_scalar('data/generator_loss', args.lambda_3 * generator_disc_loss.item(), total_steps)
+        # writer.add_scalar('data/discr_loss_ratio', discr_loss_ratio, total_steps)
+        writer.add_scalars('loss/', {
+            # 'generator': args.lambda_1 * generator_image_loss + args.lambda_2 * generator_encoded_loss + args.lambda_3 * generator_disc_loss.item(),
+            'generator': generator_disc_loss.item(),
+            'discriminator': disc_loss_real + disc_loss_gen.item(),
+        }, total_steps)
+        start = time.time()
+
 
 # TODO rebuild this plot
 # plt.figure(figsize=(10,5))
